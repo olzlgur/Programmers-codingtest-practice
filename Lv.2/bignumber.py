8bf85749b212f8fe5558786950de79e13ef78bb9
Lee Ji Heok <olzlgur@naver.com>
2022-02-21 23:12:36 +0900

    2022-02-21

---
+++ Lv.2/bignumber.py
@@ -0,0 +1,35 @@
+# 큰 수 만들기
+# 문제 설명
+# 어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
+
+# 예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 이 중 가장 큰 숫자는 94 입니다.
+
+# 문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다. number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로 return 하도록 solution 함수를 완성하세요.
+
+# 제한 조건
+# number는 1자리 이상, 1,000,000자리 이하인 숫자입니다.
+# k는 1 이상 number의 자릿수 미만인 자연수입니다.
+
+from itertools import combinations
+
+def solution(number, k):
+    answer = ''
+    numlen = len(number)
+    length = numlen - k
+    cnt = 1
+    index = 0
+    
+    for i in range(length) :
+        maxnum = '0'
+        for j in range(index, numlen - length + cnt) :
+            if number[j] == '9' :
+                maxnum = number[j] 
+                index = j + 1 
+                break
+            elif maxnum < number[j] :
+                maxnum = number[j]
+                index = j + 1
+        cnt += 1
+        answer += maxnum
+            
+    return answer
