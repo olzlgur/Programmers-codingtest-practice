ae9556531a327b722ad394bdaa514a8dcd591f84
이지혁 <olzlgur@naver.com>
2022-10-13 04:21:14 +0900

    2022-10-13

---
+++ Lv.2/operatorinsert-permutations.py
@@ -0,0 +1,29 @@
+from itertools import *
+
+def main():
+    min, max = 1000000000, 0
+    num = int(input())
+    arr = input().split()
+    temp = list(map(int, input().split()))
+    operator = setarr(temp[0], '+') + setarr(temp[1], '-') + setarr(temp[2], '*') + setarr(temp[3], '//')
+    strex = arr[0]
+    if num == 2 :
+        print(eval(arr[0]+operator[0]+arr[1]))
+        print(eval(arr[0]+operator[0]+arr[1]))
+        return
+    for tu in permutations(operator) :
+        for i in range(1, num) :
+            strex = str(eval(strex + tu[i-1] + arr[i]))
+        if eval(strex) > max :
+            max = eval(strex)
+        if eval(strex) < min:
+            min = eval(strex)
+        strex = arr[0]
+    print(max)
+    print(min)
+def setarr(a, b):
+    temparr = []
+    for i in range(a) :
+        temparr.append(b)
+    return temparr
+main()
